3b049d991f07c2772b80a4d0ad3bc7df9e7472cf
Gnimuc <qiyupei@gmail.com>
2021-07-15 01:56:05 +0900

    Add ray-tracing example for benchmarking

---
+++ examples/ray-tracing/RTInOneWeekend.jl
@@ -0,0 +1,27 @@
+using ClangCompiler
+using ClangCompiler.LLVM
+
+# source file
+src = joinpath(@__DIR__, "InOneWeekend", "main.cc")
+
+# includes
+common_incdir = joinpath(@__DIR__, "common")
+incdir = joinpath(@__DIR__, "InOneWeekend")
+
+# compilation flags
+args = get_default_args()
+push!(args, "-I$common_incdir")
+push!(args, "-I$incdir")
+
+# create compiler
+cpr = create_compiler(src, args)
+
+# generate IR
+mod = compile(cpr)
+
+# create JIT and call function
+ee = JIT(mod)
+ret = run(ee, lookup_function(mod, "main"))
+
+# clean up
+destroy(cpr)
