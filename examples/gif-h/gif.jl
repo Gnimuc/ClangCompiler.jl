8b2a4f9df96e8b70d6295a5d55571d15b239e5f8
Gnimuc <qiyupei@gmail.com>
2021-07-15 01:03:09 +0900

    Add gif-h example

---
+++ examples/gif-h/gif.jl
@@ -0,0 +1,22 @@
+using ClangCompiler
+using ClangCompiler.LLVM
+
+# source file
+src = joinpath(@__DIR__, "gif-h-demo.cpp")
+
+# compilation flags
+args = get_default_args()
+push!(args, "-I$(@__DIR__)")
+
+# create compiler
+cpr = create_compiler(src, args)
+
+# generate IR
+mod = compile(cpr)
+
+# create JIT and call function
+ee = JIT(mod)
+ret = run(ee, lookup_function(mod, "main"))
+
+# clean up
+destroy(cpr)
