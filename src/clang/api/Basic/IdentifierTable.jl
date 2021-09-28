c44edf50db0f270d37eab7116c81f676275385d4
Gnimuc <qiyupei@gmail.com>
2021-09-28 23:23:36 +0900

    Add back missing methods for `IdentifierTable`

--- src/clang/api/Basic/IdentifierTable.jl
+++ src/clang/api/Basic/IdentifierTable.jl
@@ -1,1 +1,9 @@
-# dummy
+function PrintStats(x::IdentifierTable)
+    @check_ptrs x
+    return clang_IdentifierTable_PrintStats(x)
+end
+
+function get(x::IdentifierTable, s::String)
+    @check_ptrs x
+    return IdentifierInfo(clang_IdentifierTable_get(x, s))
+end
