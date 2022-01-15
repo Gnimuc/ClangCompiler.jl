3231582f13860ca0ec410a1b42185921c185be2c
Gnimuc <qiyupei@gmail.com>
2022-01-15 12:13:34 +0900

    `LLVM_full_jll` => `Clang_jll`

--- src/ClangCompiler.jl
+++ src/ClangCompiler.jl
@@ -10,9 +10,9 @@ using Clang_jll
 using libclangex_jll
 
 # need manually add the output path below to `DYLD_LIBRARY_PATH`/`LD_LIBRARY_PATH`/`PATH`
-const libclangex_include = joinpath(libclangex_jll.artifact_dir, "include") |> normpath
+const libclangex_include = normpath(joinpath(libclangex_jll.artifact_dir, "include"))
 
-const julia_include_dir = joinpath(Sys.BINDIR, "..", "include", "julia") |> normpath
+const julia_include_dir = normpath(joinpath(Sys.BINDIR, "..", "include", "julia"))
 
 using LLVM
 using LLVM.Interop: call_function
@@ -22,7 +22,8 @@ import LLVM: dispose, name, value
 import Base: dump, string
 
 const CLANG_BIN = joinpath(Clang_jll.artifact_dir, "bin", "clang")
-const CLANG_INC = joinpath(Clang_jll.artifact_dir, "lib", "clang", string(LLVM.version()), "include")
+const CLANG_INC = joinpath(Clang_jll.artifact_dir, "lib", "clang", string(LLVM.version()),
+                           "include")
 
 include("../lib/LibClangEx.jl")
 using .LibClangEx
@@ -30,9 +31,9 @@ using .LibClangEx
 include("platform/JLLEnvs.jl")
 using .JLLEnvs
 
-function get_compiler_args(;is_cxx=true, version=v"7.1.0")
+function get_compiler_args(; is_cxx=true, version=v"7.1.0")
     args = JLLEnvs.get_default_args(is_cxx, version)
-    push!(args, "-isystem"*CLANG_INC)
+    push!(args, "-isystem" * CLANG_INC)
     is_cxx && push!(args, "-nostdinc++", "-nostdlib++")
     push!(args, "-nostdinc", "-nostdlib")
     pushfirst!(args, CLANG_BIN)  # Argv0
@@ -87,30 +88,30 @@ export jlty2llvmty
 const BOOT_COMPILER_REF = Ref{CXCompiler}()
 
 function __init__()
-@static if haskey(ENV, "CLANGCOMPILER_ENABLE_BOOT")
-    llvm_include_dir = joinpath(LLVM_full_jll.artifact_dir, "include") |> normpath
-    @assert isdir(llvm_include_dir) "failed to find LLVM include dir."
+    @static if haskey(ENV, "CLANGCOMPILER_ENABLE_BOOT")
+        clang_include_dir = normpath(joinpath(Clang_jll.artifact_dir, "include"))
+        @assert isdir(clang_include_dir) "failed to find Clang include dir."
 
-    boot_include_dir = joinpath(@__DIR__, "..", "boot") |> normpath
-    boot_src = joinpath(boot_include_dir, "boot.cpp")
+        boot_include_dir = normpath(joinpath(@__DIR__, "..", "boot"))
+        boot_src = joinpath(boot_include_dir, "boot.cpp")
 
-    args = get_compiler_args(; version=v"7.1.0")
-    push!(args, "-std=c++14")
-    Sys.isapple() && push!(args, "-stdlib=libc++")
-    push!(args, "-I$llvm_include_dir")
-    push!(args, "-I$libclangex_include")
-    push!(args, "-I$julia_include_dir")
-    push!(args, "-I$boot_include_dir")
+        args = get_compiler_args(; version=v"7.1.0")
+        push!(args, "-std=c++14")
+        Sys.isapple() && push!(args, "-stdlib=libc++")
+        push!(args, "-I$clang_include_dir")
+        push!(args, "-I$libclangex_include")
+        push!(args, "-I$julia_include_dir")
+        push!(args, "-I$boot_include_dir")
 
-    jit = LLJIT(;tm=JITTargetMachine())
-    irgen = IRGenerator(boot_src, args)
-    BOOT_COMPILER_REF[] = CXCompiler(irgen, jit)
+        jit = LLJIT(; tm=JITTargetMachine())
+        irgen = IRGenerator(boot_src, args)
+        BOOT_COMPILER_REF[] = CXCompiler(irgen, jit)
 
-    link_process_symbols(BOOT_COMPILER_REF[])
-    compile(BOOT_COMPILER_REF[])
+        link_process_symbols(BOOT_COMPILER_REF[])
+        compile(BOOT_COMPILER_REF[])
 
-    include(joinpath(@__DIR__, "boot.jl"))
-end # @static
+        include(joinpath(@__DIR__, "boot.jl"))
+    end # @static
 end
 
 end
