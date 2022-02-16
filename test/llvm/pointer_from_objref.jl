34d5f110a38e62cfafbf2b5f5164d82ea12a724e
Gnimuc <qiyupei@gmail.com>
2022-02-16 23:14:01 +0900

    Add sanity check for `pointer_from_objref`

---
+++ test/llvm/pointer_from_objref.jl
@@ -0,0 +1,30 @@
+using LLVM
+using Test
+
+@testset "pointer_from_objref" begin
+    @generated function pointer_from_objref_derived(x)
+        Context() do ctx
+            T_jlvalue = LLVM.StructType(LLVMType[];ctx)
+            T_pjlvalue = LLVM.PointerType(T_jlvalue, 0)
+            T_prjlvalue = convert(LLVMType, Any; ctx, allow_boxed = true)
+            T_pdjlvalue = LLVM.PointerType(T_jlvalue, #=AS Drived=# 11)
+            rettype = convert(LLVMType, Ptr{Cvoid}; ctx)
+            llvm_f, _ = LLVM.Interop.create_function(rettype, [T_prjlvalue])
+            mod = LLVM.parent(llvm_f)
+            func = LLVM.Function(mod, "julia.pointer_from_objref", LLVM.FunctionType(T_pjlvalue, [T_pdjlvalue]))
+            Builder(ctx) do builder
+                entry = BasicBlock(llvm_f, "entry"; ctx)
+                position!(builder, entry)
+                param = only(parameters(llvm_f))
+                param_drived = addrspacecast!(builder, param, T_pdjlvalue)
+                ret = call!(builder, func, [param_drived])
+                i64 = ptrtoint!(builder, ret, rettype)
+                ret!(builder, i64)
+            end
+
+            LLVM.Interop.call_function(llvm_f, Ptr{Cvoid}, Tuple{Any}, :x)
+        end
+    end
+    x = Ref(10)
+    @test pointer_from_objref_derived(x) == pointer_from_objref(x)
+end
